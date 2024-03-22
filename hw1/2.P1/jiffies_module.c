9b029e7b91f6f400fe5be8aaf151d899ef495616
catJam <loginerrore04@gmail.com>
2024-03-22 23:34:27 +0800

    Add jiffies_module and seconds_module to /proc

--- hw1/2.P1/jiffies_module.c
+++ hw1/2.P1/jiffies_module.c
@@ -5,16 +5,25 @@
 #include <linux/seq_file.h>
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("A /proc/jiffies module");
+MODULE_DESCRIPTION("A module that shows the current jiffie and the seconds "
+                   "since the start of the module");
 MODULE_AUTHOR("👾");
-
+static unsigned long start_jiffies;
 static int jiffies_show(struct seq_file *m, void *v) {
-  seq_printf(m, "%llu\n", (unsigned long long)jiffies);
+  seq_printf(m, "The current jiffies is %llu\n", (unsigned long long)jiffies);
   return 0;
 }
 
 static int jiffies_open(struct inode *inode, struct file *file) {
   return single_open(file, jiffies_show, NULL);
+}
+static int seconds_show(struct seq_file *m, void *v) {
+  seq_printf(m, "Already pass %lu seconds since the module installed\n",
+             (jiffies - start_jiffies) / HZ);
+  return 0;
+}
+static int seconds_open(struct inode *inode, struct file *file) {
+  return single_open(file, seconds_show, NULL);
 }
 static int __init jiffies_module_init(void) {
   // Create the /proc/jiffies entry
@@ -24,9 +33,18 @@ static int __init jiffies_module_init(void) {
       .proc_lseek = seq_lseek,
       .proc_release = single_release,
   };
-
   proc_create("jiffies", 0, NULL, &jiffies_fops);
   printk(KERN_INFO "/proc/jiffies created\n");
+  // Create the /proc/seconds
+  static const struct proc_ops seconds_fops = {
+      .proc_open = seconds_open,
+      .proc_read = seq_read,
+      .proc_lseek = seq_lseek,
+      .proc_release = single_release,
+  };
+  proc_create("seconds", 0, NULL, &seconds_fops);
+  printk(KERN_INFO "/proc/seconds created\n");
+  start_jiffies = jiffies;
   return 0;
 }
 
@@ -34,6 +52,9 @@ static void __exit jiffies_module_exit(void) {
   // Remove the /proc/jiffies entry
   remove_proc_entry("jiffies", NULL);
   printk(KERN_INFO "/proc/jiffies removed\n");
+  // Remove the /proc/seconds entry
+  remove_proc_entry("seconds", NULL);
+  printk(KERN_INFO "/proc/seconds removed\n");
 }
 
 module_init(jiffies_module_init);
--- hw1/project_docs.typ
+++ hw1/project_docs.typ
@@ -4,3 +4,49 @@
 #align(center, text(20pt)[
   *OS HW1 Programming projects*
 ])
+#align(right, [
+資工三 110590001 黃政 \
+資工三 110590004 林奕廷 \
+資工三 110590016 劉硯皓])
+= Environment
+- OS: Ubuntu 22.04
+- Kernel: 6.5.0-26-generic
+- Compiler: gcc 12.3.9
+= Ch2 project 1
+Commands
+```bash
+cd 2.P1
+make
+make install # for install module
+cat /proc/jiffies
+cat /proc/seconds
+make remove # for remove module
+make clean
+```
+Result image
+#figure(
+  image("2.P1/result.png"),
+  caption:[
+    The result for `cat /proc/jiffies` and `cat /proc/jiffies`
+  ]
+)
+
+= Ch3 project 1
+Commands
+```bash
+gcc 3.P1/osh.c -o osh
+./osh
+ls -l > out.txt
+sort < out.txt
+cat in.txt | sort -h --parallel=4
+!!
+rm out.txt
+```
+
+Result image
+#figure(
+  image("3.P1/result.png"),
+  caption:[
+    The result for `ls -l > out.txt`, `sort < out.txt`, `cat in.txt | sort -h --parallel=4`, `!!`, `rm out.txt`
+  ]
+)
